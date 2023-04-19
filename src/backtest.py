ae3ba17b6f1883afe087bd1396e5e785181acb41
Karim Farahat <karimfarahat@hotmail.com>
2023-04-19 19:16:22 +0100

    Fixed bug with index of strategy returns. Added the equal weight portfolio as a benchmark. Added return transformations and minimum number of observations to investment universe.

--- src/backtest.py
+++ src/backtest.py
@@ -23,30 +23,62 @@ class backtest:
             returns: Pandas series with multi-index of (TimeStamp, AssetID)
             burn_in: Int denoting the number of time periods used to initialise each strategy
         """
-        
-        strat_returns_dict = {}
-        
+        strat_returns_dict = {'Equal Weight' : returns.groupby('TimeStamp').mean()}
+                
         dates = features.index.get_level_values(0).unique()
         init_features = features.loc[pd.IndexSlice[:dates[burn_in-1],:],:]
-        init_returns = returns.loc[pd.IndexSlice[:dates[burn_in-1],:]]
-                     
+        
         for strat in self.strategies:
 
+            strat_obj = self.strategies[strat]            
+            
+            #trains models on transformed returns if desired by strategy
+            if strat_obj.return_transform:
+                transformed_returns = strat_obj.return_transform(returns)
+            else:
+                transformed_returns = returns
+            
+            init_returns = transformed_returns.loc[pd.IndexSlice[:dates[burn_in-1],:]]
             strat_returns = []
-            strat_obj = self.strategies[strat]
             strat_obj.process_data(init_features, init_returns)
             
-            for key, df in features.loc[pd.IndexSlice[dates[burn_in]:,:],:].groupby('TimeStamp'):
+            for key, feats in features.loc[pd.IndexSlice[dates[burn_in]:,:],:].groupby('TimeStamp'):
                 
+                #enforces minimum lookback
+                if strat_obj.risk_model.cov_lookback:
+                    rtns = returns.loc[pd.IndexSlice[:key,:]]
+                    feats, rtns = self.min_lookback(feats, rtns, strat_obj.risk_model.cov_lookback)
+                    trans_rtns = transformed_returns.loc[pd.IndexSlice[:key,:]]
+                    _, trans_rtns = self.min_lookback(feats, trans_rtns, strat_obj.risk_model.cov_lookback)
+                else:
+                    trans_rtns = transformed_returns.loc[pd.IndexSlice[:key,:]]
+
+                #computes weights, returns, and updates strategy with new info
                 rtns = returns.xs(key, drop_level = False)
-                portfolio_weights = strat_obj.compute_weights(df)
+                portfolio_weights = strat_obj.compute_weights(feats, trans_rtns)
                 portfolio_returns = (portfolio_weights * rtns).sum()
                 strat_returns.append(portfolio_returns)
-                strat_obj.process_data(df, rtns)
+                strat_obj.process_data(feats, trans_rtns.xs(key, drop_level = False))
                 
-            strat_returns_dict[strat] = pd.Series(strat_returns, index = dates)
+            strat_returns_dict[strat] = pd.Series(strat_returns, index = dates[burn_in:])
         
         self.statistics['returns'] = strat_returns_dict
+    
+    def min_lookback(self, features, returns, min_obs):
+        """
+        Reshapes the investment universe as dictated by the minimum number of observations required in the risk model
+        
+        Arguments: 
+            features: Pandas dataframe with multi-index of (TimeStamp, AssetID)
+            returns: Pandas series with multi-index of (TimeStamp, AssetID)
+            min_obs: Int denoting the minimum number of obser
+        """
+        returns = returns.unstack().iloc[-min_obs:].dropna(axis=1)
+        univ = returns.columns
+        returns = returns.stack()
+        features = features.loc[pd.IndexSlice[:, univ],:]
+        
+        return features, returns
     
     def compute_stat(self, stat_calc, stat_name):
         """
@@ -70,26 +102,25 @@ class backtest:
         """        
         stat = self.statistics[stat_name]        
         plt.bar(stat.keys(), stat.values())
-        plt.xlabel(stat_name)
+        plt.ylabel(stat_name)
                      
     def sharpe_ratio(self, returns):
         """
-        Observed returns over volatility
+        Observed returns over volatility annualised
         """
-        return np.mean(returns) / np.std(returns)
+        return (np.mean(returns) / np.std(returns)) * np.sqrt(12)
     
     def sortino_ratio(self, returns):
         """
-        Observed mean returns over downside volatility
+        Observed mean returns over downside volatility annualised
         """
         return_downside = np.minimum(0, returns)**2
         expected_downside = np.mean(return_downside)
         downside_risk = np.sqrt(expected_downside)    
-        return np.mean(returns) / downside_risk
+        return (np.mean(returns) / downside_risk) * np.sqrt(12)
 
     def max_drawdown(self, returns):
         """
-        Largest observed loss
+        Largest observed loss annualised
         """
-        return np.min(returns)
-    
+        return np.min(returns) * 12
